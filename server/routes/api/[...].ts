23c5a5e644371524797d7beea7ae5ad3b0f76f15
Loïc Herman <loic@loicherman.ch>
2024-08-09 00:27:40 +0200

    fix: add logging for reverse proxy

--- server/routes/api/[...].ts
+++ server/routes/api/[...].ts
@@ -2,22 +2,27 @@ import { getUserSessionId, requireUserSession } from 'nuxt-oidc-auth/runtime/ser
 import type { PersistentSession } from 'nuxt-oidc-auth/runtime/types/oidc.d.ts'
 import { decryptToken } from 'nuxt-oidc-auth/runtime/server/utils/security.mjs'
 import { createProxyEventHandler } from 'h3-proxy'
+import { consola } from 'consola'
 
 export default defineEventHandler(async (event) => {
   await requireUserSession(event)
   const sessionId = await getUserSessionId(event)
+
   if (!sessionId) {
+    consola.warn('Failed to get user session ID')
     throw createError({ statusCode: 401, statusMessage: 'Not Authenticated' })
   }
 
   const persistentSession = await useStorage('oidc').getItem<PersistentSession>(sessionId)
   if (!persistentSession) {
+    consola.warn('Failed to get persistent session')
     throw createError({ statusCode: 401, statusMessage: 'Not Authenticated' })
   }
 
   const { public: { apiBaseUrl }, oidcTokenKey } = useRuntimeConfig()
   const accessToken = await decryptToken(persistentSession.accessToken, oidcTokenKey)
   if (!accessToken) {
+    consola.warn('Failed to decrypt access token')
     throw createError({ statusCode: 401, statusMessage: 'Not Authenticated' })
   }
 
