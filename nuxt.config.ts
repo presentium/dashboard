f748008d2d83a79ed09644d45f5af8a4dd89995e
Loïc Herman <loic@loicherman.ch>
2024-08-11 15:11:33 +0200

    fix: full url proxy didn't send authorization headers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,7 +75,7 @@ export default defineNuxtConfig({
       api: {
         // NOTE: here it is intentional to path through /api, since we are using a proxy to the API server, which
         // appends the user access token to the requests for authentication.
-        baseURL: `${getPublicURL()}/api/v1`,
+        baseURL: `/api/v1`,
         schema: '../openapi/api-docs-v1.json',
       },
     },
--- server/routes/api/notifications.ts
+++
@@ -1,4 +0,0 @@
-// TODO [lh] remove this once we have API notifications
-export default defineEventHandler(async () => {
-  return []
-})
