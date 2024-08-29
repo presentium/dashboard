6d86af68f1d35929f017bc3feb5ed88ea132fc27
Massimo Stefani <massimo.cruz25@gmail.com>
2024-08-29 15:18:15 +0200

    feat: student page filter

--- app/pages/students.vue
+++ app/pages/students.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 const defaultColumns = [{
-  key: 'id',
-  label: '#',
-}, {
   key: 'firstName',
   label: 'Name',
   sortable: true,
@@ -14,12 +11,29 @@ const defaultColumns = [{
   key: 'email',
   label: 'Email',
   sortable: true,
+}, {
+  key: 'actions',
+  label: 'Actions',
+  sortable: false,
 }]
+
+function items(row) {
+  return [
+    [{
+      label: 'Edit',
+      icon: 'i-heroicons-pencil-square-20-solid',
+      click: () => console.log('Edit', row.id),
+    }],
+    [{
+      label: 'Delete',
+      icon: 'i-heroicons-trash-20-solid',
+    }],
+  ]
+}
 
 const input = ref<{ input: HTMLInputElement }>()
 
-const { data: students, pending } = await useApi<StudentViewModel[]>('/students')
-const q = ref('')
+const { data: students, pending } = useApi<StudentViewModel[]>('/students')
 const selected = ref<StudentViewModel[]>([])
 const selectedColumns = ref(defaultColumns)
 const sort = ref({ column: 'id', direction: 'asc' as const })
@@ -42,6 +56,17 @@ defineShortcuts({
     input.value?.input?.focus()
   },
 })
+const q = ref('')
+const filteredRows = computed(() => {
+  if (!q.value) {
+    return students.value
+  }
+  return students.value.filter((student) => {
+    return Object.values(student).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase())
+    })
+  })
+})
 </script>
 
 <template>
@@ -49,7 +74,7 @@ defineShortcuts({
     <UDashboardPanel grow>
       <UDashboardNavbar
         title="Students"
-        :badge="students.length"
+        :badge="students?.length ?? 'loading'"
       >
         <template #right>
           <UInput
@@ -92,14 +117,24 @@ defineShortcuts({
       <UTable
         v-model="selected"
         v-model:sort="sort"
-        :rows="students"
+        :rows="filteredRows"
         :columns="columns"
         :loading="pending"
         sort-mode="manual"
         class="w-full"
         :ui="{ divide: 'divide-gray-200 dark:divide-gray-800' }"
         @select="onSelect"
-      />
+      >
+        <template #name-data="{ row }">
+          <span :class="[selected.find(person => person.id === row.id) && 'text-primary-500 dark:text-primary-400']">{{ row.name }}</span>
+        </template>
+
+        <template #actions-data="{ row }">
+          <UDropdown :items="items(row)">
+            <UButton color="gray" icon="i-heroicons-ellipsis-horizontal-20-solid" />
+          </UDropdown>
+        </template>
+      </UTable>
     </UDashboardPanel>
   </UDashboardPage>
 </template>
